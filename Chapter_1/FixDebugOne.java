200787dd585bfe319409879b19488b19bdef49aa
MilesBasanta-UCM <milesb309@gmail.com>
2025-09-01 16:21:45 +0800

    Add files via upload
    my assignment for the debugging activity

---
+++ Chapter_1/FixDebugOne.java
@@ -0,0 +1,11 @@
+public class FixDebugOne{
+   public static void main(String[]args)
+   {
+     // This is program displays a meeting
+     System.out.println("Hello World!");
+   }
+}
+     // Bugs:
+     // Wrong syntax for "Systm.out.println" instead of "System.out.prinln"
+
+     // Missing ";" at the end of the statement
