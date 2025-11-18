eb69b7fe3ffaa663f639b9bbf670d1a8269ea67c
MilesBasanta-UCM <milesb309@gmail.com>
2025-11-18 21:23:05 +0800

    Add files via upload

---
+++ Chapter_7/Average.java
@@ -0,0 +1,24 @@
+public class Average {
+
+    public static int countGreater(int[] arr) {
+        int count = 0;
+        double sum = arr[0];   
+
+        for (int i = 1; i < arr.length; i++) {
+            double average = sum / i;  
+
+            if (arr[i] > average) {
+                count++;
+            }
+
+            sum += arr[i];  
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] responseTimes = {100, 200, 150, 300};
+        System.out.println(countGreater(responseTimes));  
+    }
+}
